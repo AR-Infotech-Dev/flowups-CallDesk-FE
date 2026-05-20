33d005f17f518158d00ab93c88f12548a8134c1c
AR-Infotech-DEV <arinfotechtdl@gmail.com>
2026-05-20 17:13:20 +0530

    Aniket : Kanban Added

--- src/api/config.js
+++ src/api/config.js
@@ -1,14 +1,18 @@
 export const API_SERVER_URL = import.meta.env.API_SERVER_URL || "http://localhost:3000" ;
-export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://localhost:3000/api/v1" ;
+// export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://localhost:3000/api/v1" ;
+export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://13.60.209.177/api/v1" ;
 
+// export const API_SERVER_URL = import.meta.env.API_SERVER_URL || "http://192.168.1.23:3000" ;
 // export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://192.168.1.23:3000/api/v1" ;
+// export const API_SERVER_URL = import.meta.env.API_SERVER_URL || "http://10.168.243.83:3000" ;
+// export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://10.168.243.83:3000/api/v1" ;
 
+// IN NETWORK
 
 // export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://192.168.1.23:3000/api/v1" ;
 // export const API_SERVER_URL = import.meta.env.API_SERVER_URL || "https://flowups-be.onrender.com" ;
 // export const API_BASE_URL = import.meta.env.API_BASE_URL || "https://flowups-be.onrender.com/api/v1";
 
-// IN NETWORK
 // export const API_SERVER_URL = import.meta.env.API_SERVER_URL || "http://192.168.1.23:3000" ;
 // export const API_BASE_URL = import.meta.env.API_BASE_URL || "http://192.168.1.23:3000/api/v1" ;
 
